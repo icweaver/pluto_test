ff5546423b848713a147069c8560a9c546fadbc2
icweaver <iweaver@cfa.harvard.edu>
2020-08-10 09:28:13 -0400

    added more planet subscripts for clarity

--- depth.jl
+++ depth.jl
@@ -15,6 +15,9 @@ md"Given exoplanet and host star parameters from the literature, calculate deriv
 # ╔═╡ 19b35ef4-dac3-11ea-2d25-97e5482ff6a0
 md"### Literature values"
 
+# ╔═╡ 75d6dcbe-db0a-11ea-2839-9542a238b679
+md"Source: [Exoplanet Archive](https://exoplanetarchive.ipac.caltech.edu/cgi-bin/DisplayOverview/nph-DisplayOverview?objname=HAT-P-23%20b)"
+
 # ╔═╡ 0b6821a4-dac3-11ea-27d7-911521f0d3c0
 md"### Calculate all the things"
 
@@ -26,7 +29,7 @@ begin
 end;
 
 # ╔═╡ 17302d74-d63b-11ea-3de3-49f0df0554ca
-# Input params from study
+# Input params from studies to compare
 studies = [
 	(
 		name = "Ciceri et al. (2015)",
@@ -82,13 +85,13 @@ begin
 	get_gₛ(; Mₛ, Rₛ) = G * Mₛ / Rₛ^2
 
 	# Planet surface gravity
-	get_g(; Mₚ, RₚRₛ, Rₛ) = G * Mₚ / (RₚRₛ^2 * Rₛ^2)
+	get_gₚ(; Mₚ, RₚRₛ, Rₛ) = G * Mₚ / (RₚRₛ^2 * Rₛ^2)
 
 	# Planet equilibrium temperature
 	get_Tₚ(; Tₛ, aRₛ, α) = Tₛ * (1.0 - α)^(1//4) * (0.5/aRₛ)^(1//2)
 
 	# Planet scale height
-	get_H(; μ, Tₚ, g) = k * Tₚ / (μ * g)
+	get_H(; μ, Tₚ, gₚ) = k * Tₚ / (μ * gₚ)
 
 	# Estimated signal from planet atmosphere
 	get_Delta_D(; H, RₚRₛ, Rₛ) = 2.0 * H * RₚRₛ/Rₛ
@@ -105,9 +108,9 @@ begin
 		
 		# Calculate remaining params
 		gₛ = get_gₛ(Mₛ=Mₛ, Rₛ=st.Rₛ)
-		g  = get_g(Mₚ=Mₚ, RₚRₛ=st.RₚRₛ, Rₛ=st.Rₛ)
+		gₚ  = get_gₚ(Mₚ=Mₚ, RₚRₛ=st.RₚRₛ, Rₛ=st.Rₛ)
 		Tₚ = get_Tₚ(Tₛ=st.Tₛ, aRₛ=st.aRₛ, α=st.α)
-		H  = get_H(μ=st.μ, Tₚ=Tₚ, g=g)
+		H  = get_H(μ=st.μ, Tₚ=Tₚ, gₚ=gₚ)
 		ΔD = get_Delta_D(H=H, RₚRₛ=st.RₚRₛ, Rₛ=st.Rₛ)
 
 		# Collect results
@@ -115,6 +118,8 @@ begin
 		**$(st.name):**
 
 		log gₛ (cm/s²) = $(log10(ustrip(uconvert(u"cm/s^2", gₛ))))
+		
+		log gₚ (cm/s²) = $(log10(ustrip(uconvert(u"cm/s^2", gₚ))))
 
 		ρₛ = $(uconvert(u"g/cm^3", ρₛ))
 
@@ -140,8 +145,9 @@ end
 # ╟─c9ac27ee-dac0-11ea-2a8c-2d144b034a82
 # ╟─b2286b26-dac2-11ea-1ce0-c7da562aa641
 # ╟─19b35ef4-dac3-11ea-2d25-97e5482ff6a0
+# ╟─75d6dcbe-db0a-11ea-2839-9542a238b679
 # ╠═17302d74-d63b-11ea-3de3-49f0df0554ca
 # ╟─0b6821a4-dac3-11ea-27d7-911521f0d3c0
-# ╟─3833772c-d63f-11ea-09b5-f36d68e512ea
-# ╟─3f79c516-da77-11ea-1f6b-d3e7191a95d8
-# ╟─02bfa078-d62b-11ea-15df-d701431829b9
+# ╠═3833772c-d63f-11ea-09b5-f36d68e512ea
+# ╠═3f79c516-da77-11ea-1f6b-d3e7191a95d8
+# ╠═02bfa078-d62b-11ea-15df-d701431829b9
