1696e15f9ff480c9b5b7cca47c3b2764a943ba70
icweaver <iweaver@cfa.harvard.edu>
2020-08-10 15:20:36 -0400

    trying out structs instead of namedtuples

--- exocalc.jl
+++ exocalc.jl
@@ -23,52 +23,10 @@ md"### Calculate all the things"
 
 # ╔═╡ 02bfa078-d62b-11ea-15df-d701431829b9
 begin
-	using Measurements, Unitful, UnitfulAstro, Markdown
+	using Measurements, Unitful, UnitfulAstro, Parameters, Markdown
 	using PhysicalConstants.CODATA2018: G, k_B, m_u
 	const amu, k = m_u, k_B
 end;
-
-# ╔═╡ 17302d74-d63b-11ea-3de3-49f0df0554ca
-# Input params from studies to compare
-studies = [
-	(
-		name = "Ciceri et al. (2015)",
-		μ    = 2.0*amu,
-		α    = 0.0,
-		K    = (368.5 ± 17.6)u"m/s",
-		i    = (85.74 ± 0.95)u"°",
-		P    = (1.21288287 ± 0.00000017)u"d",
-		RₚRₛ = 0.11616 ± 0.00081,
-		aRₛ  = 4.5459 ± 0.0919,
-		Tₛ   = (5885 ± 72)u"K",
-		Rₛ   = (1.089 ± 0.028)u"Rsun",
-	),
-	(
-		name = "GAIA DR2",
-		μ    = 2.0*amu,
-		α    = 0.0,
-		K    = (346.0 ± 21)u"m/s", # latest RV data, from B17
-		i    = (85.1 ± 1.5)u"°",  # latest RV data, from B17
-		P    = (1.2128867 ± 0.0000002)u"d", # latest transit data: (S&R16)
-		RₚRₛ = 0.1113 ± 0.0010, # latest transit data: (S&R16)
-		aRₛ  = 4.16 ± 0.26,
-		Tₛ   = (5734 ± 99.8735)u"K",
-		Rₛ   = (1.1858169 ± 0.0424133)u"Rsun",
-	),
-	(
-		name = "GAIA DR2 w/ DR1 mass",
-		μ    = 2.0*amu,
-		α    = 0.0,
-		K    = (346.0 ± 21)u"m/s", # latest RV data, from B17
-		i    = (85.1 ± 1.5)u"°",  # latest RV data, from B17
-		P    = (1.2128867 ± 0.0000002)u"d", # latest transit data: (S&R16)
-		Mₚ   = (1.34 ± 0.59)u"Mjup",
-		RₚRₛ = 0.1113 ± 0.0010, # latest transit data: (S&R16)
-		aRₛ  = 4.16 ± 0.26,
-		Tₛ   = (5734 ± 99.8735)u"K",
-		Rₛ   = (1.1858169 ± 0.0424133)u"Rsun",
-	),
-];
 
 # ╔═╡ 3f79c516-da77-11ea-1f6b-d3e7191a95d8
 begin
@@ -97,14 +55,83 @@ begin
 	get_Delta_D(; H, RₚRₛ, Rₛ) = 2.0 * H * RₚRₛ/Rₛ
 end;
 
+# ╔═╡ db28dbd2-db12-11ea-28e4-2b6cf30bd102
+@with_kw_noshow struct Study @deftype Union{Nothing, Quantity}
+	# Reference name (i.e. Ciceri et al. 2015)
+	name::String = "Custom"
+	
+	# Orbital params
+	RₚRₛ::Union{Measurement, Quantity} = 0 ± 0 # Planet to star radius ratio
+	aRₛ::Union{Measurement, Quantity} = 0 ± 0  # Semi-major axis to star radius ratio
+	P = nothing                                # Period
+	K = nothing                                # RV semi-amplitude
+	i = nothing                                # Inclination
+	
+	# Planet params
+	μ = nothing                                # Mean molecular weight
+	α::Union{Measurement, Quantity} = 0±0      # Albedo
+	Tₚ = nothing                               # Equilibrium temperature
+	ρₚ = nothing                               # Density
+	Mₚ = nothing                               # Mass
+	Rₚ = nothing                               # Radius
+	
+	# Star params
+	Tₛ = nothing                               # Effective temperature
+	ρₛ = nothing                               # Density
+	Mₛ = nothing                               # Mass
+	Rₛ = nothing                               # Radius
+end;
+
+# ╔═╡ 17302d74-d63b-11ea-3de3-49f0df0554ca
+# Input params from studies to compare
+studies = [
+	Study(
+		name = "Ciceri et al. (2015)",
+		μ    = 2.0*amu,
+		α    = 0.0 ± 0.0,
+		K    = (368.5 ± 17.6)u"m/s",
+		i    = (85.74 ± 0.95)u"°",
+		P    = (1.21288287 ± 0.00000017)u"d",
+		RₚRₛ = 0.11616 ± 0.00081,
+		aRₛ  = 4.5459 ± 0.0919,
+		Tₛ   = (5885 ± 72)u"K",
+		Rₛ   = (1.089 ± 0.028)u"Rsun",
+	),
+	Study(
+		name = "GAIA DR2",
+		μ    = 2.0*amu,
+		α    = 0.0 ± 0.0,
+		K    = (346.0 ± 21)u"m/s", # latest RV data, from B17
+		i    = (85.1 ± 1.5)u"°",  # latest RV data, from B17
+		P    = (1.2128867 ± 0.0000002)u"d", # latest transit data: (S&R16)
+		RₚRₛ = 0.1113 ± 0.0010, # latest transit data: (S&R16)
+		aRₛ  = 4.16 ± 0.26,
+		Tₛ   = (5734 ± 99.8735)u"K",
+		Rₛ   = (1.1858169 ± 0.0424133)u"Rsun",
+	),
+	Study(
+		name = "GAIA DR2 w/ DR1 mass",
+		μ    = 2.0*amu,
+		α    = 0.0 ± 0.0,
+		K    = (346.0 ± 21)u"m/s", # latest RV data, from B17
+		i    = (85.1 ± 1.5)u"°",  # latest RV data, from B17
+		P    = (1.2128867 ± 0.0000002)u"d", # latest transit data: (S&R16)
+		Mₚ   = (1.34 ± 0.59)u"Mjup",
+		RₚRₛ = 0.1113 ± 0.0010, # latest transit data: (S&R16)
+		aRₛ  = 4.16 ± 0.26,
+		Tₛ   = (5734 ± 99.8735)u"K",
+		Rₛ   = (1.1858169 ± 0.0424133)u"Rsun",
+	),
+];
+
 # ╔═╡ 3833772c-d63f-11ea-09b5-f36d68e512ea
 begin
 	results = []
 	for st in studies
 		# Calculate secondary params if not given
-		ρₛ = (haskey(st, :ρₛ)) ? st.ρₛ : get_ρₛ(P=st.P, aRₛ=st.aRₛ)
-		Mₛ = (haskey(st, :Mₛ)) ? st.Mₛ : get_Mₛ(ρₛ=ρₛ, Rₛ=st.Rₛ)
-		Mₚ = (haskey(st, :Mₚ)) ? st.Mₚ : get_Mₚ(K=st.K, i=st.i, P=st.P, Mₛ=Mₛ)
+		ρₛ = (isnothing(st.ρₛ)) ? get_ρₛ(P=st.P, aRₛ=st.aRₛ) : st.ρₛ
+		Mₛ = (isnothing(st.Mₛ)) ? get_Mₛ(ρₛ=ρₛ, Rₛ=st.Rₛ) : st.Mₛ
+		Mₚ = (isnothing(st.Mₚ)) ? get_Mₚ(K=st.K, i=st.i, P=st.P, Mₛ=Mₛ) : st.Mₚ
 		
 		# Calculate remaining params
 		gₛ = get_gₛ(Mₛ=Mₛ, Rₛ=st.Rₛ)
@@ -148,6 +175,7 @@ end
 # ╟─75d6dcbe-db0a-11ea-2839-9542a238b679
 # ╠═17302d74-d63b-11ea-3de3-49f0df0554ca
 # ╟─0b6821a4-dac3-11ea-27d7-911521f0d3c0
-# ╠═3833772c-d63f-11ea-09b5-f36d68e512ea
-# ╠═3f79c516-da77-11ea-1f6b-d3e7191a95d8
-# ╠═02bfa078-d62b-11ea-15df-d701431829b9
+# ╟─3833772c-d63f-11ea-09b5-f36d68e512ea
+# ╟─3f79c516-da77-11ea-1f6b-d3e7191a95d8
+# ╟─db28dbd2-db12-11ea-28e4-2b6cf30bd102
+# ╟─02bfa078-d62b-11ea-15df-d701431829b9
